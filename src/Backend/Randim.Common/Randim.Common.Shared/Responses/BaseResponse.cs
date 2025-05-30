0490543a99d07549cbade3bc939b1fda4a312ff9
GiorgiUer <guffera2005@gmail.com>
2025-05-30 18:12:38 +0400

    structure

---
+++ src/Backend/Randim.Common/Randim.Common.Shared/Responses/BaseResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Randim.Common.Shared.Responses;
+
+public class BaseResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public List<string>? Message { get; set; }
+    public bool IsSuccess { get; set; }
+}
+public class BaseResponse<T> : BaseResponse
+{
+    public T? Data { get; set; }
+}
